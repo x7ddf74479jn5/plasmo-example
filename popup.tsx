54f47f9bae13fb0f485ab2fdb7248664986d8fff
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-03-19 02:46:22 +0900

    ✨ feat: popup ui

--- popup.tsx
+++ popup.tsx
@@ -1,19 +1,24 @@
 import "./style.css"
 
+import { Container, Select } from "@mantine/core"
+
 function IndexPopup() {
   document.body.style.width = "15rem"
   document.body.style.height = "15rem"
 
   return (
-    <div className="flex h-screen items-center justify-center">
-      <p>どの言語に翻訳しますか？</p>
-      <select name="target_lang">
-        <option value="EN">英語</option>
-        <option value="KO">韓国語</option>
-        <option value="ZH">中国語</option>
-        <option value="JA">日本語</option>
-      </select>
-    </div>
+    <Container p="xl">
+      <Select
+        label="どの言語に翻訳しますか？"
+        defaultValue="EN"
+        data={[
+          { value: "EN", label: "英語" },
+          { value: "KO", label: "韓国語" },
+          { value: "ZH", label: "中国語" },
+          { value: "JA", label: "日本語" }
+        ]}
+      />
+    </Container>
   )
 }
 
