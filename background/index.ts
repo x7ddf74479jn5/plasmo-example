9af95bdc855e3184dda5cc6f9d0e8967ba3b7d73
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-03-19 18:25:08 +0900

    ✨ feat: add SW

---
+++ background/index.ts
@@ -0,0 +1,19 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "translation",
+    title: "選択したテキストを翻訳",
+    contexts: ["selection"]
+  })
+})
+
+chrome.contextMenus.onClicked.addListener(async (info, tab) => {
+  if (!!tab) {
+    switch (info.menuItemId) {
+      case "translation":
+        console.log(info.selectionText)
+        break
+    }
+  }
+})
+
+export {}
